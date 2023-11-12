ebb86fd880d84d94963d248b045246ef56d0484c
woojin <wjin0728@naver.com>
2023-11-13 01:52:20 +0900

    현재의 게임 씬을 스택에서 제거하는  pop_mode 함수 추가

--- game_framework.py
+++ game_framework.py
@@ -19,6 +19,19 @@ def push_mode(mode):
     mode.init()
 
 
+def pop_mode():
+    global stack
+    if (len(stack) > 0):
+        # execute the current mode's finish function
+        stack[-1].finish()
+        # remove the current mode
+        stack.pop()
+
+    # execute resume function of the previous mode
+    if (len(stack) > 0):
+        stack[-1].resume()
+
+
 def run(start_mode):
     global running, stack
     running = True
