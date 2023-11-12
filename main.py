1e9d519d9637634387cbdc6265254aa1fda604a3
woojin <wjin0728@naver.com>
2023-11-13 06:41:35 +0900

    main play 씬 클래스 구성

---
+++ main.py
@@ -0,0 +1,12 @@
+import pico2d
+import game_framework
+
+
+import play_mode
+
+pico2d.open_canvas(1536, 864)
+game_framework.run(start_mode)
+pico2d.close_canvas()
+
+
+# 2022180031 이우진
---
+++ play_,mode.py
@@ -0,0 +1,36 @@
+from pico2d import *
+import game_framework
+import random
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+
+
+def init():
+    pass
+
+
+def finish():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    clear_canvas()
+    update_canvas()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
