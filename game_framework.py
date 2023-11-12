9d7831ad8f2c662f7145c4bb9027044954e3df1f
woojin <wjin0728@naver.com>
2023-11-13 01:41:35 +0900

    중간에 게임 씬 변경할 수 있는 change_mode 함수 추가

--- game_framework.py
+++ game_framework.py
@@ -1,5 +1,16 @@
 
 
+def change_mode(mode):
+    global stack
+    if (len(stack) > 0):
+        # execute the current mode's finish function
+        stack[-1].finish()
+        # remove the current mode
+        stack.pop()
+    stack.append(mode)
+    mode.init()
+
+
 def run(start_mode):
     global running, stack
     running = True
