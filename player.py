7cae03375214fbf9a5170499918417778c33a606
woojin <wjin0728@naver.com>
2023-11-14 21:56:36 +0900

    플레이어의 동작을 제어할 상태기계 클래스 구현

--- player.py
+++ player.py
@@ -3,14 +3,40 @@ import game_framework
 from pico2d import *
 
 # Player Action Speed
-TIME_PER_ACTION = 0.5
+TIME_PER_ACTION = 1.0
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 6
 
 
+class StateMachine:
+    def __init__(self, boy):
+        self.boy = boy
+        self.cur_state
+        self.transitions = {}
+
+    def start(self):
+        self.cur_state.enter(self.boy, ('NONE', 0))
+
+    def update(self):
+        self.cur_state.do(self.boy)
+
+    def handle_event(self, e):
+        for check_event, next_state in self.transitions[self.cur_state].items():
+            if check_event(e):
+                self.cur_state.exit(self.boy, e)
+                self.cur_state = next_state
+                self.cur_state.enter(self.boy, e)
+                return True
+
+        return False
+
+    def draw(self):
+        self.cur_state.draw(self.boy)
+
+
 class Player:
     def __init__(self):
-        self.x, self.y = 50, 90
+        self.x, self.y = 50, 350
         self.frame = 0
         self.action = 3
         self.face_dir = 1
